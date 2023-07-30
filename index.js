d27594210fc768cf8b7ddc8730dab76192e16aa9
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-07-30 19:38:03 +0100

    update

---
+++ index.js
@@ -0,0 +1,98 @@
+
+
+var calculate = document.getElementById('calc');
+var reset = document.getElementById('reset');
+
+var cName = document.getElementById('cName');
+var fName = document.getElementById('fName');
+var dBrith = document.getElementById('dBirth');
+
+var lpResult = document.getElementById('lpResult');
+var lpText = document.getElementById('lpText');
+
+var bnResult = document.getElementById('bnResult');
+var bnText = document.getElementById('bnText');
+
+var bdnResult = document.getElementById('bdnResult');
+var bdnText = document.getElementById('bdnText');
+
+var ednResult = document.getElementById('ednResult');
+var ednText = document.getElementById('ednText');
+
+var suhdResult = document.getElementById('suhdResult');
+var suhdText = document.getElementById('suhdText');
+
+var pnResult = document.getElementById('pnResult');
+var pnText = document.getElementById('pnText');
+
+var mnResult = document.getElementById('mnResult');
+var mnText = document.getElementById('mnText');
+
+var anResult = document.getElementById('anResult');
+var anText = document.getElementById('anText');
+
+var lpebResult = document.getElementById('lpebResult');
+var lpebText = document.getElementById('lpebText');
+
+var hdpbResult = document.getElementById('hdpbResult');
+var hdpbText = document.getElementById('hdpbText');
+
+var rtnResult = document.getElementById('rtnResult');
+var rtnText = document.getElementById('rtnText');
+
+var blnResult = document.getElementById('blnResult');
+var blnText = document.getElementById('blnText');
+
+var ssnResult = document.getElementById('ssnResult');
+var ssnText = document.getElementById('ssnText');
+
+var kdnResult = document.getElementById('kdnResult');
+var kdnText = document.getElementById('kdnText');
+
+var kclResult = document.getElementById('kclResult');
+var kclText = document.getElementById('kclText');
+
+var hpResult = document.getElementById('hpResult');
+var hpText = document.getElementById('hpText');
+
+var hnnResult = document.getElementById('hnnResult');
+var hnnText = document.getElementById('hnnText');
+
+var ppoeResult = document.getElementById('ppoeResult');
+var ppoeText = document.getElementById('ppoeText');
+
+var mpeResult = document.getElementById('mpeResult');
+var mpeText = document.getElementById('mpeText');
+
+var ipeResult = document.getElementById('ipeResult');
+var ipeText = document.getElementById('ipeText');
+
+var epeResult = document.getElementById('epeResult');
+var epeText = document.getElementById('epeText');
+
+var cstResult = document.getElementById('cstResult');
+var cstText = document.getElementById('cstText');
+
+var pynResult = document.getElementById('pynResult');
+var pynText = document.getElementById('pynText');
+
+var ecResult = document.getElementById('ecResult');
+var ecText = document.getElementById('ecText');
+
+var ttcResult = document.getElementById('ttcResult');
+var ttcText = document.getElementById('ttcText');
+
+var pcnResult = document.getElementById('pcnResult');
+var pcnText = document.getElementById('pcnText');
+
+var pnnResult = document.getElementById('pnnResult');
+var pnnText = document.getElementById('pnnText');
+
+var clnResult = document.getElementById('clnResult');
+var clnText = document.getElementById('clnText');
+
+
+calculate.addEventListener('click', ()=> {
+    
+
+})
